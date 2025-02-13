8be995b925366da1957f18b34b5e2cb45f43be17
har2ha <HarshaA504@stemcollegiate.ca>
2025-02-13 09:33:55 -0700

    Create Feb_13.py

---
+++ Feb_13.py
@@ -0,0 +1,7 @@
+ # for day 1 I learned about the print command 
+print("Harsha Anil")
+print("Feburary 13th 2025")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling 😌")
