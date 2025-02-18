995efa167b839ef081c7707111a4232b8a8b0d42
har2ha <HarshaA504@stemcollegiate.ca>
2025-02-18 14:56:36 -0700

    Create Feb_18.py

---
+++ Feb_18.py
@@ -0,0 +1,13 @@
+# Today I learned about variables and that things don't always go the way you want them to
+urName = input("What's your name?: ")
+urFood = input("What's your favorite food?: ")
+print("Omg that sounds good!😋")
+urHome = input("Where do you live?:")
+print("That's a cool place to live in!")
+print("Your name is")
+print(urName)
+print("and you like")
+print(urFood) 
+print("and you live in")
+print(urHome)
+print("You're super cool!😎")
