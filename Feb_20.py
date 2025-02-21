9c93ace4370d1eaffe7d6637aaae6b3d62feb451
har2ha <HarshaA504@stemcollegiate.ca>
2025-02-21 10:14:02 -0700

    Create Feb_20.py

---
+++ Feb_20.py
@@ -0,0 +1,9 @@
+#Today I learned about how to combine things
+food= input("Give me a type of food:" )
+plant= input("Give me a type of plant:" )
+cooking= input("Give me a method of cooking:" )
+burned= input("Give me a word to describe burned food:" )
+item= input("Give me a household item:" )
+print("YOUR CREATION IS READY!")
+print("Enjoy your", cooking, food, "with", burned, plant, "with a side of", item)
+print("BON APPETIT!")
