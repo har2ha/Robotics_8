959562001e57fa8e96fe61da18ff3434dc9fbe38
har2ha <HarshaA504@stemcollegiate.ca>
2025-03-13 09:07:27 -0600

    Create Mar_13.py

---
+++ Mar_13.py
@@ -0,0 +1,14 @@
+# Day 6 Challenge 
+# Today I learned about the elif statment 
+print("LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "Ava" and password == "avaaaaaa":
+ print("Hey Ava! How've you been?")
+elif username == "Ben" and password == "BEN10!":
+ print("Hey dude! It's been a while!")
+elif username == "Jack" and password == "1234567890":
+ print("Oh.It's you......")
+else:
+ print("WHO ARE YOU?!")
