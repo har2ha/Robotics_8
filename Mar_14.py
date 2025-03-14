7f107aa9b2e9db04d9729c492a64d3f059eb9e7e
har2ha <HarshaA504@stemcollegiate.ca>
2025-03-13 23:57:21 -0600

    Create Mar_14.py

---
+++ Mar_14.py
@@ -0,0 +1,11 @@
+# Day 7 Challenge 
+# Today I learned that identing my if statements is very important and can mess up my whole code if done wrong. 
+first= input("Are you a true fan of the show 'Wednesday'?") 
+if first == "Yes": 
+  answer=input("Alright, what actor plays the main character of the show?") 
+  if answer == "Jenna Ortega":
+    print("Thats's right! You really are a true fan!")
+  else: 
+    print("Wrong answer, try again.")
+else:
+  print("See ya.") 
